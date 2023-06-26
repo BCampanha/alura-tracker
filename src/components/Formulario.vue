3402774c954b8230b3c976648d7cc8786b3e3186
Beatriz Campanha Silva <bcampanha@usp.br>
2023-06-26 15:49:27 -0300

    alinhando botões

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -2,28 +2,26 @@
   <div class="box">
     <div class="columns">
       <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
-        <input 
-          type="text"
-          class="input"
-          placeholder="Qual tarefa você deseja iniciar?"
-        />
+        <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?" />
       </div>
       <div class="column">
-        <section>
-          <strong>00:00:00</strong>
-        </section>
-        <button class="button">
-          <span class="icon">
-            <i class="fas fa-play"></i>
-          </span>
-          <span>play</span>
-        </button>
-        <button class="button">
-          <span class="icon">
-            <i class="fas fa-stop"></i>
-          </span>
-          <span>stop</span>
-        </button>
+        <div class="is-flex is-align-items-center is-justify-content-space-between">
+          <section>
+            <strong>00:00:00</strong>
+          </section>
+          <button class="button">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
       </div>
     </div>
   </div>
@@ -33,6 +31,6 @@
 import { defineComponent } from 'vue';
 
 export default defineComponent({
-  name: 'Formulario'
+  name: 'AluraFormulario'
 })
 </script>
