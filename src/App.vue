35f0a4dc155bb66da134656d93ef010c67d66f53
Beatriz Campanha Silva <bcampanha@usp.br>
2023-06-29 09:23:10 -0300

    Ouvindo e executando a troca de tema!

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
-  <main class="columns is-gapless is-multiline">
+  <main class="columns is-gapless is-multiline" :class="{ 'modo-escuro': modoEscuroAtivo }">
     <div class="column is-one-quarter">
-      <BarraLateral />
+      <BarraLateral @aoTemaAlterado="trocarTema"/>
     </div>
     <div class="column is-three-quarter conteudo">
       <AluraFormulario @aoSalvarTarefa="salvarTarefa"/>
@@ -37,7 +37,8 @@ export default defineComponent({
   },
   data() {
     return {
-      tarefas: [] as ITarefa[]
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false
     }
   },
   computed: {
@@ -48,6 +49,9 @@ export default defineComponent({
   methods: {
     salvarTarefa (tarefa: ITarefa) {
       this.tarefas.push(tarefa)
+    },
+    trocarTema(modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo
     }
   }
 });
@@ -65,7 +69,7 @@ main {
 }
 main.modo-escuro {
   --bg-primario: black;
-  --bg-secundario: indigo;
+  --bg-secundario: brown;
   --bg-terciario: midnightblue;
   --texto-primario: lavenderblush;
 }
