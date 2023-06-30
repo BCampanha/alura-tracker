7d65dfa9694d25ffac24ce0d091e217fe4b33c3a
Beatriz Campanha Silva <bcampanha@usp.br>
2023-06-30 14:40:11 -0300

    Adicionando formulário e recebendo inputs

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -1,11 +1,53 @@
 <template>
-  <h1>Projetos</h1>
+  <section class="projetos">
+    <h3>Projetos</h3>
+    <form class="mt-3" @submit.prevent="salvar">
+        <div class="form-group">
+          <label for="nomeDoProjeto">Nome do projeto</label>
+          <input
+            type="text"
+            class="form-control"
+            v-model="nomeDoProjeto"
+            id="nomeDoProjeto"
+            placeholder="Insira o nome do projeto"
+          />
+        </div>
+      <div class="d-flex justify-content-end mt-3">
+        <button type="submit" class="btn btn-primary">Salvar</button>
+      </div>
+    </form>
+  </section>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import IProjeto from '../interfaces/IProjeto'
+
 export default defineComponent({
-  name: 'Projetos'
+  name: "Projetos",
+  data() {
+    return {
+      nomeDoProjeto: "",
+      projetos: [] as IProjeto[]
+    };
+  },
+  methods: {
+    salvar() {
+      const projeto: IProjeto = {
+        nome: this.nomeDoProjeto,
+        id: new Date().toISOString()
+      }
+      this.projetos.push(projeto)
+      this.nomeDoProjeto = ''
+    }
+  }
 })
 
 </script>
+
+<style scoped>
+.projetos {
+  padding-top: 1.25rem;
+  color: var(--texto-primario);
+}
+</style>
