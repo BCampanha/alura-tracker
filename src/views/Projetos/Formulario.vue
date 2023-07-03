cb1c1c6bc4053b084fc2e97a5b9350e13ca1be10
Beatriz Campanha Silva <bcampanha@usp.br>
2023-07-03 13:42:04 -0300

    implementamos a modificação da lista

--- src/views/Projetos/Formulario.vue
+++ src/views/Projetos/Formulario.vue
@@ -25,6 +25,17 @@ import { useStore } from '@/store'
 
 export default defineComponent({
   name: "Formulario",
+  props: {
+    id: {
+      type: String
+    }
+  },
+  mounted() {
+    if(this.id) {
+      const projeto = this.store.state.projetos.find(proj => proj.id == this.id)
+      this.nomeDoProjeto = projeto?.nome || ''
+    }
+  },
   data() {
     return {
       nomeDoProjeto: ""
@@ -32,7 +43,16 @@ export default defineComponent({
   },
   methods: {
     salvar() {
-      this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+      if (this.id) {
+        // EDIÇÃO
+        this.store.commit('ALTERA_PROJETO', {
+          id: this.id,
+          nome: this.nomeDoProjeto
+        })
+      } else {
+        // Novo projeto
+        this.store.commit('ADICIONA_PROJETO', this.nomeDoProjeto)
+      }
       this.nomeDoProjeto = '';
       this.$router.push('/projetos')
     }
