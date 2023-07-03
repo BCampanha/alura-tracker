9c50c205dd0627c6e17312646f8be24da5600920
Beatriz Campanha Silva <bcampanha@usp.br>
2023-07-03 13:22:55 -0300

    estética

--- src/views/Projetos.vue
+++ src/views/Projetos.vue
@@ -4,19 +4,19 @@
     <RouterLink
       to="/projetos/novo"
       class="btn botao">
-      <span><i class="fa fa-plus"></i></span>
+      <span><i class="fa fa-plus"></i> Novo projeto</span>
     </RouterLink>
     <ul class="list-group list-group-flush mt-4">
       <li class="list-group-item d-flex tabela">
         <div class="col fw-semibold">ID</div>
         <div class="col fw-semibold">Nome</div>
-        <div class="col fw-semibold">Ações</div>
+        <div class="col-1 fw-semibold">Ações</div>
       </li>
       <tr></tr>
       <li class="list-group-item d-flex tabela" v-for="projeto in projetos" :key="projeto.id">
         <div class="col">{{ projeto.id }}</div>
         <div class="col">{{ projeto.nome }}</div>
-        <div class="col">
+        <div class="col-1">
           <RouterLink
             :to="`/projetos/${projeto.id}`"
             class="btn btn-sm botao">
