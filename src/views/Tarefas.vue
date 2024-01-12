0dd799809cfd031fc7e9a5235d8d00202009f5f9
Beatriz Campanha <bcampanha@usp.br>
2024-01-12 12:32:10 -0300

    migrando as tarefas para a store

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -13,13 +13,13 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
-import ITarefa from "../interfaces/ITarefa";
 import Box from "../components/Box.vue"
 import { useStore } from '@/store';
-import { ADICIONA_TAREFA } from '@/store/tipo-mutacoes';
+import { OBTER_TAREFAS } from '@/store/tipo-acoes';
+import ITarefa from '@/interfaces/ITarefa';
 
 export default defineComponent({
   name: 'App',
@@ -30,24 +30,26 @@ export default defineComponent({
   },
   data() {
     return {
-      tarefas: [] as ITarefa[],
       modoEscuroAtivo: false
     }
   },
   computed: {
     listaEstaVazia(): boolean {
-      return this.tarefas.length === 0
+      return this.tarefas.length == 0
     }
   },
   methods: {
-    salvarTarefa (tarefa: ITarefa) {
+    salvarTarefa (tarefa: ITarefa) : void {
       this.tarefas.push(tarefa)
-      this.store.commit(ADICIONA_TAREFA, tarefa)
+      //this.store.commit(ADICIONA_TAREFA, tarefa)
       }
     },
   setup() {
     const store = useStore()
-    return {
+    store.dispatch(OBTER_TAREFAS)
+
+    return { 
+      tarefas: computed( () => store.state.tarefas),
       store
     }
   }
